84307729882104d634ab46f7ca34cfad031daa9f
coko0324 <coko0324@gmail.com>
2019-02-09 17:28:34 +0900

    編集機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,10 +17,20 @@ class ItemsController < ApplicationController
     redirect_to @item
   end
 
+  def edit
+    @item =Item.find(params[:id])
+  end
+
+  def update
+    @item =Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to @item
+  end
+
   private
 
   def item_params
-    parames.require(:item).permit(:name,:price,:seller,:description,:email,:image_url)
+    params.require(:item).permit(:name,:price,:seller,:description,:email,:image_url)
   end
 
 
